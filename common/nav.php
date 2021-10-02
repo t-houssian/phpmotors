e3dff565eefbbe207e35107fc19f8e8a2f75a3c0
t-houssian <tylerhoussian@gmail.com>
2021-10-02 14:17:42 -0600

    Enhancement 1

--- common/nav.php
+++ common/nav.php
@@ -1,10 +1,12 @@
 <div id="grid-2">
-    <nav id="nav">
-        <li class="nav-item"><a href="#">Home</a></li>
-        <li class="nav-item"><a href="#">Classic</a></li>
-        <li class="nav-item"><a href="#">Sports</a></li>
-        <li class="nav-item"><a href="#">SUV</a></li>
-        <li class="nav-item"><a href="#">Trucks</a></li>
-        <li class="nav-item"><a href="#">Used</a></li>
+    <nav>
+        <ul id="nav">
+            <li class="nav-item"><a href="#">Home</a></li>
+            <li class="nav-item"><a href="#">Classic</a></li>
+            <li class="nav-item"><a href="#">Sports</a></li>
+            <li class="nav-item"><a href="#">SUV</a></li>
+            <li class="nav-item"><a href="#">Trucks</a></li>
+            <li class="nav-item"><a href="#">Used</a></li>
+        </ul>
     </nav>
 </div>
